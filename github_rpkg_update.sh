63b654b344843ec702b1efc05559149cf0cfff88
Changxu Fan <changxu.fan@outlook.com>
2023-11-26 17:47:28 -0600

    regular update

---
+++ github_rpkg_update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+########
+echo "you would still need to run ssh-add before you update"
+#  eval "$(ssh-agent -s)"
+#  ssh-add ~/.ssh/id_ed25519_github
+#######
+
+for i in abaFanc abaFanc2 bamFanc cageFanc common downloadSRX liteRnaSeqFanc scFanc v4c; do
+	cd ~/R_packages/${i}
+	# git remote set-url origin git@github.com:ChangxuFan/${i}.git
+	git add -A
+	git commit -m 'regular update'
+	git push
+	cd -
+done
+
+for i in R_for_bash scripts; do
+	cd ~/${i}
+	# git remote set-url origin git@github.com:ChangxuFan/${i}.git
+	git add -A
+	git commit -m 'regular update'
+	git push
+	cd -
+done
